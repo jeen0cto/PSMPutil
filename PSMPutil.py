41ab90fa9f0de86f1679ede865aee325bcd52871
Jeen0cto <67688168+jeen0cto@users.noreply.github.com>
2020-09-16 10:26:04 +0200

    Update PSMPutil.py
    You can generate a PuTTY registry file for a list of users with the same servers list and domain.

--- PSMPutil.py
+++ PSMPutil.py
@@ -1,4 +1,4 @@
-# PSMPUtil 2020-06-28 - ver 1.0 
+# PSMPUtil 2020-09-16 - ver 1.1
 # author: Jeen0cto
 #
 # Description
@@ -7,7 +7,6 @@
 #                       username@cyberarkaccount#domain@fqdn_server@PSMP_ipaddress
 #
 # All parameters are in PSMPutil.yaml configuration file
-#
 # Please report bug to: jeen0cto@gmail.com
 
 import re
@@ -15,71 +14,71 @@ import yaml
 from colorama import init, Fore, Back, Style
 init()
 
-#print(Fore.RED + 'some red text')
-#print(Back.GREEN + 'and with a green background')
-#print(Style.DIM + 'and in dim text')
 print(Style.BRIGHT)
-#print('back to normal now')
-
 print(Fore.YELLOW + '*** PSMP Util - ver 1.0 ***')
 print()
 
-
 # Load configuration file (YAML) 
 print(Fore.GREEN + 'Loading configuration file...')
-file = open('PSMPutil.yaml', 'r')
+file = open('PSMPutil2.yaml', 'r')
 cfg = yaml.load(file, Loader=yaml.FullLoader)
 
 # PSMP Section
 section = cfg['psmp']
 PSMPHost = section['host']
-# Users Section
-section = cfg['users']
-username = section['username']
-account = section['account']
 domain = section['domain']
+
+# Users Section (it's a list!)
+users = cfg['users']
+
 # Input Section
 section = cfg['input']
 serverlist_filename = section['serverlist_filename']
 puttytemplate_filename = section['puttytemplate_filename']
+
 # Output Section
 section = cfg['output']
-outfile = username + "_" + section['filename']
 print('Done.')
-print()
-
-print('Create Putty Session registry file: ' + Fore.WHITE + outfile)
-print(Fore.GREEN + 'Username : ' + Fore.WHITE + username)
-print(Fore.GREEN + 'Account : ' + Fore.WHITE + account)
-print(Fore.GREEN + 'Server list file : ' + Fore.WHITE + serverlist_filename)
-print(Fore.GREEN + 'PSM Proxy IP: ' + Fore.WHITE + PSMPHost)
 print()
 
 # Open file server list
 serversf = open(serverlist_filename,"r")
-# Create registry output file
-outregf = open(outfile,'w')
-
-# connection string part one
-part1 = username + '@' + account + '#' + domain + '@'
 
 d = {}
-for srv in serversf:
-    servername = srv.splitlines()[0]
-    hostname = part1 + servername + '.' + domain + '@' + PSMPHost
-    d['servername'] = srv.splitlines()[0] + '_PSMP'
-    d['hostname'] = hostname
+for usr in users:
+    username = usr['username']
+    account = usr['account']
+
+    # Create registry output file
+    outfile = username + "_" + section['filename']
+    outregf = open(outfile,'w')
+
+    # connection string part one
+    part1 = username + '@' + account + '#' + domain + '@'
+
+    print(Fore.YELLOW  + 'Create Putty Session registry file: ' + Fore.WHITE + outfile)
+    print(Fore.GREEN + 'Username : ' + Fore.WHITE + username)
+    print(Fore.GREEN + 'Account : ' + Fore.WHITE + account)
+    print(Fore.GREEN + 'Server list file : ' + Fore.WHITE + serverlist_filename)
+    print(Fore.GREEN + 'PSM Proxy IP: ' + Fore.WHITE + PSMPHost)
     
-    templatef = open(puttytemplate_filename,'r')
-    for line in templatef:
-        if re.findall(r"<server>", line):
-            line = line.replace('<server>', srv.splitlines()[0] + '_PSMP')
-        if re.findall(r"<hostname>", line):
-            line = line.replace('<hostname>', hostname)
-        outregf.write(line)
-    templatef.close()
+    for srv in serversf:
+        servername = srv.splitlines()[0]
+        hostname = part1 + servername + '.' + domain + '@' + PSMPHost
+        d['servername'] = srv.splitlines()[0] + '_PSMP'
+        d['hostname'] = hostname
+        
+        templatef = open(puttytemplate_filename,'r')
+        for line in templatef:
+            if re.findall(r"<server>", line):
+                line = line.replace('<server>', srv.splitlines()[0] + '_PSMP')
+            if re.findall(r"<hostname>", line):
+                line = line.replace('<hostname>', hostname)
+            outregf.write(line)
+        templatef.close()
+    print(Fore.YELLOW  + 'Closing output file: ' + Fore.WHITE +  outfile)
+    print(Fore.YELLOW + '')
+    outregf.close()
 
 serversf.close()
-outregf.close()
-print(Fore.GREEN + 'Closing output file: ' + Fore.WHITE +  outfile)
 print(Fore.GREEN + 'Done.')
